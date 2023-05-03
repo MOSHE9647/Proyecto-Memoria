91d81de9fb8dbcf7d3249d47185ee2fdbc23143e
Moshe9647 <62158167+MOSHE9647@users.noreply.github.com>
2023-05-02 20:52:32 -0600

    init commit

---
+++ main.c
@@ -0,0 +1,85 @@
+#include "src/socios.h"
+
+Memory mainMemory;
+
+int main () {
+	int option = 0;
+	do {
+		system("clear");
+		printf("Proyecto Administración de Memoria\n");
+		printf("**************************************\n");
+		printf("1. Mapas de Bits\n");
+		printf("2. Listas Ligadas\n");
+		printf("3. Sistemas Socios\n");
+		printf("4. Multiprogramacion\n");
+		printf("5. Salir del Programa\n");
+		printf("**************************************\n");
+		printf("Digite una Opcion: "); scanf("%d", &option);
+
+		switch (option) {
+			case 1: {
+				//mapaDeBits();
+
+				break;
+			}
+			case 2: {
+				int option = 0;
+				do {
+					system("clear");
+					printf("Politica: Listas Ligadas\n");
+					printf("**************************************\n");
+					printf("1. Siguiente Ajuste\n");
+					printf("2. Primer Ajuste\n");
+					printf("3. Mejor Ajuste\n");
+					printf("4. Peor Ajuste\n");
+					printf("5. Volver\n");
+					printf("**************************************\n");
+					printf("Digite una Opcion: "); scanf("%d", &option);
+
+					switch (option) {
+						case 1:
+							//sigAjuste();
+							break;
+						case 2:
+							//primAjuste();
+							break;
+						case 3:
+							//mejorAjuste();
+							break;
+						case 4:
+							//peorAjuste();
+							break;
+						case 5:
+							break;
+						default:
+							printf("\nOpcion Invalida!!\n");
+							printf("Digite una Opcion Valida...\n\n");
+							sysPause();
+							break;
+					}
+				} while (option != 5);
+				break;
+			}
+			case 3: {
+				system("clear");
+				socios(&mainMemory);
+				break;
+			}
+			case 4: {
+				//multiProgram();
+				break;
+			}
+			case 5: {
+				printf("\nSaliendo del Programa...\n");
+				break;
+			}
+			default: {
+				printf("\nOpcion Invalida!!\n");
+				printf("Digite una Opcion Valida...\n\n");
+				sysPause();
+				break;
+			}
+		}
+	} while (option != 5);
+	return 1;
+}
---
+++ src/memoria.h
@@ -0,0 +1,65 @@
+#ifndef MEMORIA_H
+#define MEMORIA_H
+
+/***********************************************************
+    ARCHIVO DE CABECERA QUE CONTIENE TODA LA INFORMACIÓN
+    RELACIONADA A LOS PROCESOS, MEMORIA Y SUS PARTICIONES
+************************************************************/
+
+#include <semaphore.h>   /* Para uso de Semáforos    */
+#include <pthread.h>     /* Para uso de los Hilos    */
+#include <stdlib.h>      /* Para uso de Rand y Srand */
+#include <signal.h>      /* Funcion que lee CTRL+Z   */
+#include <unistd.h>      /* Para uso de Sleep        */
+#include <stdio.h>       /* Para Entrada / Salida    */
+
+#define MEMORY_SIZE 256  /* Memoria Total a Utilizar        */
+#define NUM_PROCESS 5	 /* Cantidad de Procesos a Utilizar */
+#define FALSE 0          /* Definimos FALSE como 0          */
+#define TRUE 1           /* Definimos TRUE como 1           */
+
+// ESTRUCTURAS:
+/* Estructura para los Procesos */
+typedef struct {
+    int id;     /* ID del Proceso     */
+    int size;   /* Tamaño del Proceso */
+} Process;
+
+/* Estructura para las Particiones */
+typedef struct {
+    int id;             /* ID de la Particiión       */
+    int size;           /* Tamaño de la Partición    */
+    int isFree;         /* Indica si está libre      */
+    Process *process;   /* Para almacenar el Proceso */
+} Partition;
+
+/* Estructura para la Memoria */
+typedef struct {
+    Partition partitions[MEMORY_SIZE];
+    int numPartitions;
+    int size;
+} Memory;
+
+/* Función para Pausar el Programa */
+void sysPause() {
+    /****************************************
+        Esta función se encarga de simular
+        el system("pause") que se utiliza
+        en Windows
+    *****************************************/
+	printf("Press enter to continue...");
+	fflush(stdin);
+	int c = getchar();
+	getchar();
+}
+/* Función para cerrar el Programa */
+void sigint_handler(int sig) {
+    /************************************************
+        Esta función se ejecuta cuando el programa
+        detecta que se presionó CTRL+C
+    *************************************************/
+    printf("\nPrograma detenido por el usuario.\n");
+    exit(0);
+}
+
+#endif
